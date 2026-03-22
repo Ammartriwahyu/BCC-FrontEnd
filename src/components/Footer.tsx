d5841ca199ec998e9ee21629c42aa4770b695990
Ammar Triwahyu <ammartriwahyu@gmail.com>
2026-03-22 12:54:33 +0700

    feat: initial commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,29 @@
 export default function Footer() {
   return (
-    <footer className="border-t mt-20">
+    <footer className="w-full bg-[#E4D9D4] flex justify-between items-center px-[76px] py-[104px]">
 
-      <div className="max-w-6xl mx-auto px-6 py-10 text-center text-sm text-gray-600">
+      <div>
+        <h1 className="text-white font-bold text-[36px] font-Poppins">
+          ReadWrite
+        </h1>
+      </div>
 
-        <p>
-          © {new Date().getFullYear()} ReadWrite. All rights reserved.
-        </p>
+      <div className="flex flex-col gap-4 w-[415px]">
+
+        <div className="flex items-center gap-3 text-white">
+         
+          <span>@lorem ipsum</span>
+        </div>
+
+        <div className="flex items-center gap-3 text-white">
+   
+          <span>0812-6767-9987</span>
+        </div>
+
+        <div className="flex items-center gap-3 text-white">
+     
+          <span>ReadWrite@gmail.com</span>
+        </div>
 
       </div>
 
